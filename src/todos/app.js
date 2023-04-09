0baea6ed1d3fb04c0fc9ebbeb6c06ed3d250345a
Zhenrong Yuan <zhenrong.yuanlu@gmail.com>
2023-04-09 16:06:02 -0400

    render-todos.js

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,5 +1,10 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store';
+import { renderTodos } from './use-cases';
 
+const ElementIDs = {
+    TodoList: '.todo-list',
+}
 
 /**
  * Aquí se crea lo que queremos renderizar en pantalla.
@@ -7,12 +12,18 @@ import html from './app.html?raw';
  */
 export const App = ( elementId ) => {
 
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+        renderTodos( ElementIDs.TodoList, todos );
+    }
+
  // Esta es una función anónima auto invocada. Se va a llamar cuando la función app se llama.
  
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append( app );
+        displayTodos();
     })();
 
 
