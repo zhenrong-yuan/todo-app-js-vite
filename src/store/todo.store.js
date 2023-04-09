82fdebb42e780cbbff7946f7fa8e292ae5a70103
Zhenrong Yuan <zhenrong.yuanlu@gmail.com>
2023-04-09 13:49:47 -0400

    Models Todo added.

---
+++ src/store/todo.store.js
@@ -0,0 +1,27 @@
+import { Todo } from '../todos/models/todo.model';
+
+const Filters = {
+    All: 'all',
+    Completed: 'Completed',
+    Pending: 'Pending'
+}
+
+const state = {
+    todos: [
+        new Todo('Piedra del alma'),
+        new Todo('Piedra del infinito'),
+        new Todo('Piedra del tiempo'),
+    ],
+    filter: Filters.All,
+}
+
+
+const initStore = () => {
+    console.log(state);
+    console.log('InitStore *-*');
+}
+
+
+export default {
+    initStore,
+}
