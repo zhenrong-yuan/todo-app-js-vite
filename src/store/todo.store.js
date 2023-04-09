07fb8bb3d26b2bce8ed891c2f1c43ecec6bab64b
Zhenrong Yuan <zhenrong.yuanlu@gmail.com>
2023-04-09 15:23:26 -0400

    Implementation: getTodos, addTodo, deleteTodo, deleteCompleted, setFilter, getCurrentFilter.

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -21,7 +21,73 @@ const initStore = () => {
     console.log('InitStore *-*');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented.');
+}
+
+const getTodos = ( filter = Filters.All ) => {
+    switch ( filter ) {
+        case Filters.All:
+            return [...state.todos];
+
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+
+        case Filters.Completed:
+            return state.todos.filter( todo => !todo.done );
+             
+        default:
+            throw new Error(`Option ${ filter } is not valid.`)
+            break;
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => { 
+    
+    if ( !description ) throw new Error('Description is required');
+    state.todos.push( new Todo(description) );
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = () => {
+    state.todos = state.todo.filter( todo => todo.id !== todoId );
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todo.filter( todo => todo.done );
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
 
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
